43e7c90d9cd365e25a48f797b725aa5f6c2dae69
Andre <andre.eduardo11@hotmail.com>
2022-05-15 23:40:30 -0300

    Criando estilos globais

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,20 @@
+export default {
+  colors: {
+    primary: "#4299E1",
+
+    secondary: "#63B3ED",
+    secondary_light: "rgba(99,179,237,0.3)",
+
+    success: "#12a454",
+    success_light: "rgba(18,164,84,0.5)",
+
+    attention: "#e83f4b",
+    attention_light: "rgba(232,63,91,0.5)",
+
+    shape: "#ffffff",
+    title: "#363f5f",
+    text: "#969cb2",
+    text_dark: "#000000",
+    background: "#f0f2f5",
+  }
+}
