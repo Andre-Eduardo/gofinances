5d9354dc2640a6848158bc7e07c07ef4ba244135
Andre <andre.eduardo11@hotmail.com>
2022-05-20 20:54:52 -0300

    RectButton e BorderlessButton

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -5,6 +5,7 @@ import { Feather } from '@expo/vector-icons'
 import { getBottomSpace, getStatusBarHeight } from 'react-native-iphone-x-helper'
 
 import { DataListProps } from '.'
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 export const Container = styled.View`
   flex:1;
@@ -54,6 +55,7 @@ color:${({ theme }) => theme.colors.shape} ;
 font-size:${RFValue(18)}px  ;
 font-family:${({ theme }) => theme.fonts.bold} ;
 `;
+export const LogoutButton = styled.TouchableOpacity``;
 export const Icon = styled(Feather)`
 color:${({ theme }) => theme.colors.secondary};
 font-size:${RFValue(24)}px;
