3ffa38f3758ef7909d7e763d925b6403d743d9d5
Andre <andre.eduardo11@hotmail.com>
2022-05-22 12:41:58 -0300

    Interface de Autenticação

--- App.tsx
+++ App.tsx
@@ -14,6 +14,7 @@ import {
 import theme from './src/global/styles/theme'
 import { StatusBar } from 'expo-status-bar';
 import { AppRoutes } from './src/routes/app.routes';
+import { SignIn } from './src/screens/SignIn';
 
 
 export default function App() {
@@ -31,7 +32,7 @@ export default function App() {
     <ThemeProvider theme={theme}>
       <StatusBar style='light' />
       <NavigationContainer>
-        <AppRoutes />
+        <SignIn />
       </NavigationContainer>
     </ThemeProvider>
   );
