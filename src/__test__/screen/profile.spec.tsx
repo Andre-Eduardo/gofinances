536069b55085e5659e95dc2f2a14b4998f1251e4
Andre <andre.eduardo11@hotmail.com>
2022-06-04 16:13:17 -0300

    Testes com Contexto

--- src/__test__/screen/profile.spec.tsx
+++ src/__test__/screen/profile.spec.tsx
@@ -2,22 +2,23 @@ import React from 'react'
 import { render } from '@testing-library/react-native'
 
 import { Profile } from '../../screens/Profile'
+describe('Profile', () => {
+  it('check if show correctly user input name placeholder', () => {
+    const { getByPlaceholderText } = render(<Profile />)
+    const inputName = getByPlaceholderText('Nome')
+    expect(inputName).toBeTruthy();
+  })
+  it('check if user data has been loaded', () => {
+    const { getByTestId } = render(<Profile />)
 
-test('check if show correctly user input name placeholder', () => {
-  const { getByPlaceholderText } = render(<Profile />)
-  const inputName = getByPlaceholderText('Nome')
-  expect(inputName).toBeTruthy();
-})
-test('check if user data has been loaded', () => {
-  const { getByTestId } = render(<Profile />)
+    const inputName = getByTestId('input-name')
+    const inputSurName = getByTestId('input-surname')
+    expect(inputName.props.value).toEqual('Rafael')
+  })
 
-  const inputName = getByTestId('input-name')
-  const inputSurName = getByTestId('input-surname')
-  expect(inputName.props.value).toEqual('Rafael')
-})
-
-test('check if title render correctly', () => {
-  const { getByTestId } = render(<Profile />)
-  const textTitle = getByTestId('text-title')
-  expect(textTitle.props.children).toContain('Perfil')
+  it('check if title render correctly', () => {
+    const { getByTestId } = render(<Profile />)
+    const textTitle = getByTestId('text-title')
+    expect(textTitle.props.children).toContain('Perfil')
+  })
 })
