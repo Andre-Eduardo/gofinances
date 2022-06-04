f929f6907c6db39e29a8f15a8b6047de2905788d
Andre <andre.eduardo11@hotmail.com>
2022-06-03 22:27:14 -0300

    Seletores

--- src/__test__/screen/profile.spec.tsx
+++ src/__test__/screen/profile.spec.tsx
@@ -4,6 +4,20 @@ import { render } from '@testing-library/react-native'
 import { Profile } from '../../screens/Profile'
 
 test('check if show correctly user input name placeholder', () => {
-  const { debug } = render(<Profile />)
-  debug()
+  const { getByPlaceholderText } = render(<Profile />)
+  const inputName = getByPlaceholderText('Nome')
+  expect(inputName).toBeTruthy();
+})
+test('check if user data has been loaded', () => {
+  const { getByTestId } = render(<Profile />)
+
+  const inputName = getByTestId('input-name')
+  const inputSurName = getByTestId('input-surname')
+  expect(inputName.props.value).toEqual('Rafael')
+})
+
+test('check if title render correctly', () => {
+  const { getByTestId } = render(<Profile />)
+  const textTitle = getByTestId('text-title')
+  expect(textTitle.props.children).toContain('Perfil')
 })
